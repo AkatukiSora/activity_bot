2ffbdd061010be5144da335e16e1be4e33a30299
AkatukiSora <akatuki-sora@akatuki-host.com>
2024-11-05 13:35:31 +0900

    style: ログのイニシャライザを整形

--- src/initializer/log.ts
+++ src/initializer/log.ts
@@ -1,16 +1,26 @@
-import log4js from "log4js"
+import log4js from "log4js";
 
 log4js.configure({
-    appenders: {
-      out: { type: 'stdout' },
-      lowlogs: { type: 'dateFile', filename: 'log/application.log', pattern: '.yyyy-MM-dd', compress: false, },
-      highlogs: { type: 'dateFile', filename: 'log/error.log', pattern: '.yyyy-MM-dd', compress: false, },
-      lowFilter: { type: 'logLevelFilter', appender: 'lowlogs', level: 'debug' },
-      highFilter: { type: 'logLevelFilter', appender: 'highlogs', level: 'warn' },
+  appenders: {
+    out: { type: "stdout" },
+    lowlogs: {
+      type: "dateFile",
+      filename: "log/application.log",
+      pattern: ".yyyy-MM-dd",
+      compress: false,
     },
-    categories: {
-      default: { appenders: ['out', "lowFilter", "highFilter"], level: 'all' }
-    }
-  })
+    highlogs: {
+      type: "dateFile",
+      filename: "log/error.log",
+      pattern: ".yyyy-MM-dd",
+      compress: false,
+    },
+    lowFilter: { type: "logLevelFilter", appender: "lowlogs", level: "debug" },
+    highFilter: { type: "logLevelFilter", appender: "highlogs", level: "warn" },
+  },
+  categories: {
+    default: { appenders: ["out", "lowFilter", "highFilter"], level: "all" },
+  },
+});
 
-export const logger = log4js.getLogger()
+export const logger = log4js.getLogger();
